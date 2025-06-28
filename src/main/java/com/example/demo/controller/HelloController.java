f3097afa002e449686782493daf08c6fe5241cbe
NAVYASRI VADDE <navyasri.vadde.tech@gmail.com>
2025-06-28 16:18:52 -0700

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class HelloController {
 	
 	@GetMapping("/hello")
-	public String sayHello() {
+	public Stri sayHello() {
 		return "Hello programmer!";
 	}
 }
